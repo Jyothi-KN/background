7ffafb4d531cb4f251c22db7c7ee129f11167bf8
Jyothi-KN <92202672+Jyothi-KN@users.noreply.github.com>
2022-02-15 16:01:40 +0530

    Add files via upload

---
+++ s4.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <body>
+        Ans: Yes. Coronavirus health insurance comes with a waiting period of either 16 days or 30 days from the date of commencement of the policy. Therefore, it is advisable to check with your insurance provider regarding any initial waiting period.
+        </body>
+    </head></html>
